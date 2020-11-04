025beeb274bb374e56067ec0631f930578cfc7ab
HanSatCode <seki02@naver.com>
2020-11-04 23:10:44 +0900

    업로드 테스트

---
+++ backup.js
@@ -0,0 +1,5 @@
+var setCookie = function(name, value, exp) {
+    var date = new Date();
+    date.setTime(date.getTime() + exp*24*60*60*1000);
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+}; // 쿠키제작
