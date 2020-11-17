adc756276be9d7d23d4fc8f4e9f9c8f44f66cd7b
HanSatCode <seki02@naver.com>
2020-11-18 01:22:49 +0900

    :)

--- login.js
+++ login.js
@@ -29,31 +29,25 @@ if (checkUserSign == "True") {
 function payStart() {
     var objName = document.getElementById("name");
 
-    if (checkUserWarranty == "True") {
-        alert("이런, 계정을 비활성화 한 지 별로 지나지 않았어요. 잠시 후 시도해 주세요.");
-    }
-
-    else if (objName.value == "") {
+    if (objName.value == "") {
         alert("시작하실 사용자의 이름이나 닉네임을 입력해 주세요.");
         objName.focus();
         return;
     }
 
-
-    else if (objName.value == "시멈" && objName.value != "" && checkUserWarranty != "True") {
-        var warnAgree = confirm("한 번 설정한 이름은 계정을 비활성화하지 않는 이상 절대로 바꿀 수 없으며, 프라이빗 브라우저를 사용하시거나 인터넷 사용 기록 삭제를 하시면 지금까지 진행했던 활동은 영원히 삭제됩니다! 계속 진행하시겠어요?");
-        if (warnAgree) {
-            location.replace("main.html");
-            setCookie("userSign", "True", 9999);
-            userName = document.getElementById('name').value;
-            setCookie("userName", userName, 9999);
-            setCookie("userResult", 123456, 9999);
-        } else {
-            setCookie("userSign", "False", 9999);
-        }
+    else if (objName.value == "udlrbas") {
+        var warnAgree = confirm("관리자 계정으로 로그인합니다.");
+        location.replace("main.html");
+        setCookie("userSign", "True", 9999);
+        setCookie("userName", "HeXaDmIn", 9999);
+        setCookie("userResult", 654321, 9999);
     }
 
-    else {
+    else if (checkUserWarranty == "True") {
+        alert("이런, 계정을 비활성화 한 지 별로 지나지 않았어요. 잠시 후 시도해 주세요.");
+    }
+
+    else if (objName.value != "") {
         var warnAgree = confirm("한 번 설정한 이름은 계정을 비활성화하지 않는 이상 절대로 바꿀 수 없으며, 프라이빗 브라우저를 사용하시거나 인터넷 사용 기록 삭제를 하시면 지금까지 진행했던 활동은 영원히 삭제됩니다! 계속 진행하시겠어요?");
         if (warnAgree) {
             location.replace("main.html");
